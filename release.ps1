dcbafbc4f2bed1b3b176c09a8027ecfd1bbabfad
Dietrich Epp <depp@zdome.net>
2014-04-29 23:51:57 -0700

    Use correct runtime DLL

--- release.ps1
+++ release.ps1
@@ -10,6 +10,6 @@ cp README.txt Oubliette
 cp LICENSE.txt Oubliette
 cp -r data Oubliette\Data
 rm Oubliette\Data\.gitignore, Oubliette\Data\quant.sh
-cp 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll' Oubliette
+cp 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll' Oubliette
 cp $sdl\lib\x86\*.dll,$sdl_image\lib\x86\*.dll,$sdl_image\lib\x86\*.txt,$glew\bin\Release\Win32\glew32.dll Oubliette
 & $7zip a Oubliette.zip Oubliette
