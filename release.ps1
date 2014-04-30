76b8297de9ee1d390506c914f567eb61574b6f15
Dietrich Epp <depp@zdome.net>
2014-04-29 22:53:10 -0700

    Write script to package Windows build

---
+++ release.ps1
@@ -0,0 +1,15 @@
+$sdl = "C:\SDL2-2.0.3"
+$sdl_image = "C:\SDL2_image-2.0.0"
+$glew = "C:\glew-1.10.0"
+$7zip = "C:\Program Files (x86)\7-Zip\7z.exe"
+
+$ErrorActionPreference = "Stop"
+mkdir Oubliette
+cp Release\Oubliette.exe Oubliette
+cp README.txt Oubliette
+cp LICENSE.txt Oubliette
+cp -r data Oubliette\Data
+rm Oubliette\Data\.gitignore, Oubliette\Data\quant.sh
+cp 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll' Oubliette
+cp $sdl\lib\x86\*.dll,$sdl_image\lib\x86\*.dll,$sdl_image\lib\x86\*.txt,$glew\bin\Release\Win32\glew32.dll Oubliette
+& $7zip a Oubliette.zip Oubliette
