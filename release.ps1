20d02abb3b390b35b851ab98706da92f36f5497b
Dietrich Epp <depp@zdome.net>
2014-05-13 23:46:20 -0700

    Package DLLs in subdirectory
    It is apparently impossible to change the icon on the resulting
    shortcut to bundled icon (i.e., use a relative path).  I am also unaware
    of how to hide the command window when using the shortcut.

--- release.ps1
+++ release.ps1
@@ -6,11 +6,13 @@ $7zip = "C:\Program Files (x86)\7-Zip\7z.exe"
 
 $ErrorActionPreference = "Stop"
 mkdir Oubliette
-cp Release\Oubliette.exe Oubliette
+cp Oubliette.lnk Oubliette
 cp README.txt Oubliette
 cp LICENSE.txt Oubliette
 cp -r data Oubliette\Data
 rm Oubliette\Data\.gitignore, Oubliette\Data\quant.py
-cp 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll','C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll' Oubliette
-cp $sdl\lib\x86\*.dll,$sdl_image\lib\x86\*.dll,$sdl_image\lib\x86\*.txt,$sdl_mixer\lib\x86\*.dll,$sdl_mixer\lib\x86\*.txt,$glew\bin\Release\Win32\glew32.dll Oubliette
+mkdir Oubliette\Win32
+cp Release\Oubliette.exe Oubliette\Win32
+cp 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll','C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll' Oubliette\Win32
+cp $sdl\lib\x86\*.dll,$sdl_image\lib\x86\*.dll,$sdl_image\lib\x86\*.txt,$sdl_mixer\lib\x86\*.dll,$sdl_mixer\lib\x86\*.txt,$glew\bin\Release\Win32\glew32.dll Oubliette\Win32
 & $7zip a Oubliette.zip Oubliette
